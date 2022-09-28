c9a17166fc6f14d327c394d847dcdb31177f2fa6
minkatan <minka.tan@gmail.com>
2022-09-28 11:10:56 -0400

    update svg

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
-    },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
+  // devServer: {
+  //   static: {
+  //     directory: path.resolve(__dirname, 'dist'),
+  //   },
+  //   port: 3000,
+  //   open: true,
+  //   hot: true,
+  //   compress: true,
+  //   historyApiFallback: true,
+  // },
   module: {
     rules: [
       {
